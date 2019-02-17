6865e39069ba5cc7a09987ed897ed23272f6899b
mikhio <mikhail.iogansen@gmail.com>
2019-02-17 20:47:51 +0300

    Updates

---
+++ precache-manifest.8df7f42e0d8b3e007f0983bdaf10f68e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a56124487faaa7f1ca6b",
+    "url": "/life-react/static/js/runtime~main.a5612448.js"
+  },
+  {
+    "revision": "4a9e9a0b69733ced1e48",
+    "url": "/life-react/static/js/main.4a9e9a0b.chunk.js"
+  },
+  {
+    "revision": "6ce0bc0bdfa45803dc40",
+    "url": "/life-react/static/js/1.6ce0bc0b.chunk.js"
+  },
+  {
+    "revision": "4a9e9a0b69733ced1e48",
+    "url": "/life-react/static/css/main.390bc3f7.chunk.css"
+  },
+  {
+    "revision": "5958c5f18a314b1402d19ce1575709a5",
+    "url": "/life-react/index.html"
+  }
+];
